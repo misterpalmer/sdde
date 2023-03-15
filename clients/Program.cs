9608199e98ef02b2ddd5d327e7b712684094bc11
Matthew Andrew <mandrew333@msn.com>
2023-03-14 21:07:57 -0700

    feature: added console project for debugging

---
+++ clients/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Udub.Sdde.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+ISinglyLinkedList<int> tester = new SinglyLinkedList<int>();
+for (int index = 0; index < 10; index++)
+{
+    tester.AddLast(new SinglyNode<int>(index));
+}
+
+Console.WriteLine($"Total Nodes: {tester.Count.ToString()}");
+foreach(var element in tester)
+{
+    Console.WriteLine($"Node: {element}");
+}
+
+tester.Clear();
+for (int index = 0; index < 10; index++)
+{
+    tester.AddFirst(new SinglyNode<int>(index));
+}
+
+Console.WriteLine($"Total Nodes: {tester.Count.ToString()}");
+foreach(var element in tester)
+{
+    Console.WriteLine($"Node: {element}");
+}
+
+Console.WriteLine($"Total Nodes: {tester.Count.ToString()}");
+IEnumerator<int> looper = tester.GetEnumerator();
+while(looper.MoveNext())
+{
+    Console.WriteLine($"Node: {looper.Current}");
+}
+
+
+var message = Console.ReadLine();
