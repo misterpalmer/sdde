8e9040bbbe15e67ef553adb90498c9962cdc8a49
Matthew Andrew <mandrew333@msn.com>
2023-03-14 21:51:12 -0700

    feature: added tests for DoublyLinkedList

---
+++ tests/DoublyLinkedListTests.cs
@@ -0,0 +1,14 @@
+using Udub.Sdde.Collections.Generic;
+
+
+namespace sdde.tests;
+
+public class DoublylyLinkedListTests
+{
+    private readonly ITestOutputHelper output;
+    public DoublylyLinkedListTests (ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+    
+}
